d9fefd107c1176b936c613ce1ef9b36369f937fb
Eugene Vlasov <eugvv@altlinux.org>
2017-04-28 16:49:05 +0500

    Смена шрифта для руского по умолчанию в includes.lua

---
+++ bg2eer/includes.lua
@@ -0,0 +1,13 @@
+Infinity_DoFile("util")
+Infinity_DoFile("BGEE")
+
+local lua_files = Infinity_GetFilesOfType("lua")
+
+-- Run any M_ lua files.  Mods can include these to run after BGEE has run
+for k, v in ipairs(lua_files) do
+  if (v[1]):sub(1, 2):upper() == "M_" then
+    Infinity_DoFile(v[1])
+  end
+end
+
+SetPrivateProfileString('Fonts','ru_RU','')
